cc4c431a980013e38b3015702e171630e8fc0eab
Robert Greenawalt <robert.greenawalt29@gmail.com>
2020-11-23 11:27:01 -0500

    edit user interface layout

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
-# Work-Scheduler
-
 # Work-Day-Scheduler
 
-This project is an online schedule which can be utilized for note taking, keeping track of relevant meeting times, etc. The input fields will change color based on the time of day; if the hour has already passed it will be grey, if it is present the input field will be red, and if the hour is in the future the input field will be green. 
+This project is an online schedule which can be utilized for note taking, keeping track of relevant meeting times, etc. The input fields will change color based on the time of day; if the hour has already passed it will be grey, if it is present the input field will be white, and if the hour is in the future the input field will be green. 
 
-Notes can be entered into the input fields and saved with the blue save button; if the user refreshes the page, their notes will still be stored in the input field.
+Notes can be entered into the input fields and saved with the save button and cleared with the clear button; if the user refreshes the page, their notes will still be stored in the input field.
 
 
 https://rsg71.github.io/Work-Scheduler/
 
-<img src="Work Day Scheduler.gif" alt="Work Scheduler gif">
+<img src="imgs/scheduler.PNG" alt="Work Scheduler gif">
--- script.js
+++ script.js
@@ -1,24 +1,57 @@
 
 function saveInfo(event) {
     event.preventDefault();
-    localStorage.setItem($(this).prev().attr("id"), $(this).prev().val());
+    // var note = $(".form-group [name=note]").val()
+    var note = $(this).prev().val();
+    console.log(note)
+
+
+    //we want the key to be the id and the value to be whatever we type in the box
+    localStorage.setItem($(this).data("num"), note);
+
 
 }
 
+// $(document).on("submit", "form.form-group", function(event) {
+//     event.preventDefault();
+
+//     var id = $(this).data("num");
+    
+//     console.log('form submitted')
+//     console.log(`${id} submitted`)
+// })
+
+// $(".saveBtn").on("click", function(event) {
+//     event.preventDefault();
+//     var id = $(this).data("num")
+//     console.log(id)
+
+// })
 
 
 
+
+//this function will write the local storage values into the input fields
 $("input").each(function () {
     var currentId = $(this).attr("id");
     $("#" + currentId).val(localStorage.getItem(currentId));
 });
 
+
 $(document).on("click", ".saveBtn", saveInfo);
 
 
 
+function Clear(event) {
+    // event.preventDefault();
+    // console.log($(this).data("num"))
+    localStorage.removeItem($(this).data("num"))
+}
 
-//function to change the color of the input fields
+$(document).on("click", ".clearBtn", Clear)
+
+
+// //function to change the color of the input fields
 function timingColor() {
     var currentHour = moment().format("HH");
 
